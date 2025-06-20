0f1164394ca141dccd0b4486a32460e88c8654b3
Muhammad Anas <136823365+muhammadanas123618@users.noreply.github.com>
2025-06-20 20:04:47 +0500

    Delete report_generator.py

--- report_generator.py
+++
@@ -1,7 +0,0 @@
-from jinja2 import Environment , FileSystemLoader
-import os 
-def generate_html_report(data,output_path):
-    env = Environment(loader=FileSystemLoader('templates'))
-    template = env.get_template('report_template.html')
-    with open(output_path, 'w') as f:
-        f.write(template.render(**data))
